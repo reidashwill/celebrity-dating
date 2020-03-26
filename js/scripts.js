5f7f660fbb9c3a5f4d644169662a8ae1e8d3cf1b
Reid Ashwill <reidashwill@gmail.com>
2020-03-26 11:50:35 -0700

    alert function added if section left blank

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,52 @@ $(document).ready(function() {
     var gender = $("input:radio[name=gender]:checked").val();
     var drink = $("input:radio[name=drink]:checked").val();
     var height = $("input:radio[name=height]:checked").val();
-    if(gender === 'men') {
+    if(gender === 'men' && drink === 'ale' && height === 'short') {
+      $('#uhura').hide();
+      $('#crusher').hide();
+      $('#dobby').show();
+      $('#jareth').hide();
+      $('#hagrid').hide();
+      $('#hoggle').hide();
+      $('#rosie').hide();
+      $('#galadriel').hide();
+    } else if(gender === 'men' && drink === 'ale' && height === 'tall') {
+      $('#uhura').hide();
+      $('#crusher').hide();
+      $('#dobby').hide();
+      $('#jareth').hide();
+      $('#hagrid').show();
+      $('#hoggle').hide();
+      $('#rosie').hide();
+      $('#galadriel').hide();
+    } else if(gender === 'men' && drink === 'scotch' && height === 'short') {
+      $('#uhura').hide();
+      $('#crusher').hide();
+      $('#dobby').hide();
+      $('#jareth').hide();
+      $('#hagrid').hide();
+      $('#hoggle').show();
+      $('#rosie').hide();
+      $('#galadriel').hide();
+    } else if(gender === 'men' && drink === 'scotch' && height === 'tall') {
+      $('#uhura').hide();
+      $('#crusher').hide();
+      $('#dobby').hide();
+      $('#jareth').show();
+      $('#hagrid').hide();
+      $('#hoggle').hide();
+      $('#rosie').hide();
+      $('#galadriel').hide();
+    } else if(gender === 'women' && drink === 'ale' && height === 'short') {
+      $('#uhura').hide();
+      $('#crusher').hide();
+      $('#dobby').hide();
+      $('#jareth').hide();
+      $('#hagrid').hide();
+      $('#hoggle').hide();
+      $('#rosie').show();
+      $('#galadriel').hide();
+    } else if(gender === 'women' && drink === 'ale' && height === 'tall') {
       $('#uhura').hide();
       $('#crusher').hide();
       $('#dobby').hide();
@@ -12,9 +57,18 @@ $(document).ready(function() {
       $('#hagrid').hide();
       $('#hoggle').hide();
       $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
+      $('#galadriel').show();
+    } else if(gender === 'women' && drink === 'scotch' && height === 'short') {
       $('#uhura').hide();
+      $('#crusher').show();
+      $('#dobby').hide();
+      $('#jareth').hide();
+      $('#hagrid').hide();
+      $('#hoggle').hide();
+      $('#rosie').hide();
+      $('#galadriel').hide();
+    } else if(gender === 'women' && drink === 'scotch' && height === 'tall') {
+      $('#uhura').show();
       $('#crusher').hide();
       $('#dobby').hide();
       $('#jareth').hide();
@@ -22,60 +76,8 @@ $(document).ready(function() {
       $('#hoggle').hide();
       $('#rosie').hide();
       $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
-    } else if(gender ==='women') {
-      $('#uhura').hide();
-      $('#crusher').hide();
-      $('#dobby').hide();
-      $('#jareth').hide();
-      $('#hagrid').hide();
-      $('#hoggle').hide();
-      $('#rosie').hide();
-      $('#galadriel').hide();
+    } else {
+      alert('you forgot a section!');
     }
   });
 });
